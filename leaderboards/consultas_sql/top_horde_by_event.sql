4168f9ae2bc8e4df229bf24dcdead2252f1e74ee
ZyroEolu-sk <shengkai.zhu@alumnos.upm.es>
2026-03-04 10:20:03 +0100

    add: scripts sql para top horde y user_statistics

---
+++ leaderboards/consultas_sql/top_horde_by_event.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ed97a8056226b503f358f2464a3e9a3280867b2cefff62a8939aea9364e5b92
+size 257
---
+++ leaderboards/consultas_sql/user_statistics_by_dungeon.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:85d35c31acc945fabf03b515e222e560423f55ac9b0e78388fa03bbe76c0ecb4
+size 61
