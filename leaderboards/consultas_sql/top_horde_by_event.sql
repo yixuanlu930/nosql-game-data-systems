a0747a564c696c0cf1408bcec28cbb22d365b5f2
ZyroEolu-sk <shengkai.zhu@alumnos.upm.es>
2026-03-16 15:17:54 +0100

    fix: quitado una redundancia del join

--- leaderboards/consultas_sql/top_horde_by_event.sql
+++ leaderboards/consultas_sql/top_horde_by_event.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b8cd9b91c8f4463dd91b62c4e750ca1814f8c0f689e5d49c0f8492241eef0c5
-size 297
+oid sha256:4984560cc1a5ebdb51ced80df51052a7c974a9b522dbbda4aa7366c6012db17a
+size 249
